fee0a26aa181be86faa0a67bcdf5283dc6ac1314
Alex4343434343 <alexkiprip@gmail.com>
2026-01-26 08:41:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+password = input("Введите пароль: ")
+
+
+def has_digit(password):
+	return any(letter.isdigit() for letter in password)
+
+
+def is_very_long(password):
+	return len(password) > 12
+
+
+def has_upper_letters(password):
+	return any(letter.isupper() for letter in password)
+	
+
+def has_lower_letters(password):
+	return any(letter.islower() for letter in password)
+
+
+def has_symbols(password):
+
+	symbols = [
+		"!","@","#","%","^","&","*","(",")","-","=","+",
+		"_","№","{","}","[","]","?","/","|","\\",",",".","<",">"
+	]
+	
+	return any(letter in symbols for letter in password)
+
+
+functions = [
+	has_digit, 
+	is_very_long, 
+	has_upper_letters, 
+	has_lower_letters, 
+	has_symbols
+]
+
+
+score = 0
+for function in functions:
+	if function(password):
+		score = score + 2
+
+
+print(score)
