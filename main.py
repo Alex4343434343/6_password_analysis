e664137e92fa1066ae30102327d4e6aa662f8d61
Alex4343434343 <alexkiprip@gmail.com>
2026-01-27 22:43:34 +0300

    Откорректировано с помощью Flake8

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
 def has_digit(password):
-	return any(letter.isdigit() for letter in password)
+    return any(letter.isdigit() for letter in password)
 
 
 def is_very_long(password):
-	return len(password) > 12
+    return len(password) > 12
 
 
 def has_upper_letters(password):
-	return any(letter.isupper() for letter in password)
-	
+    return any(letter.isupper() for letter in password)
+
 
 def has_lower_letters(password):
-	return any(letter.islower() for letter in password)
+    return any(letter.islower() for letter in password)
 
 
-def has_symbols(password):	
-	return any(not letter.isalnum() for letter in password)
+def has_symbols(password):
+    return any(not letter.isalnum() for letter in password)
 
 
 def main():
-	password = input("Введите пароль: ")
+    password = input("Введите пароль: ")
 
-	functions = [
-		has_digit, 
-		is_very_long, 
-		has_upper_letters, 
-		has_lower_letters, 
-		has_symbols
-	]
+    functions = [
+        has_digit,
+        is_very_long,
+        has_upper_letters,
+        has_lower_letters,
+        has_symbols
+    ]
+
+    score = 0
+    for function in functions:
+        if function(password):
+            score = score + 2
+
+    print(score)
 
 
-	score = 0
-	for function in functions:
-		if function(password):
-			score = score + 2
-
-
-	print(score)
-	
 if __name__ == '__main__':
-	main()
+    main()
